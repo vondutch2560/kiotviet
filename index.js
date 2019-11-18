9dc276a58852a7006c47622d5e16e3d630dbe91d
Vondutch2560 homePC <trannguyenhai89@gmail.com>
2019-11-18 22:48:59 +0700

    Add function getCategories

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fetch = require("node-fetch");
 const fs    = require("fs");
 
+const folderGetData   = './getdata';
+const fileAccesstoken = folderGetData + '/accesstoken.txt';
+const fileCategories  = folderGetData + '/categories.csv';
+
 async function getAccessToken(){
   const clientId     = '18325940-db11-4ac5-bb1e-bbcafe005ba6';
   const clientSecret = '974FE7232C10209EA1A4A51F84FE9615864FC253';
@@ -11,25 +15,27 @@ async function getAccessToken(){
   let dataJson = await response.json();
 
   dataJson['expires_time'] = (Date.now() + 86000);
-  fs.writeFile('accesstoken.txt', JSON.stringify(dataJson), err => {
+  
+  if(!fs.existsSync(folderGetData))
+    fs.mkdirSync(folderGetData);
+
+  fs.writeFile(fileAccesstoken, JSON.stringify(dataJson), err => {
     if(err) throw err;
   });
   return dataJson;
 }
 
 async function readAccessTokenFile(){
-  if(!fs.existsSync('./accesstoken.txt')){
+  if(!fs.existsSync(fileAccesstoken)){
     let dataToken = await getAccessToken();
     return dataToken;
   }
-
-  let dataToken = await promiseReadfile();
-  return dataToken;
+  return await promiseReadfile(fileAccesstoken);
 }
 
-function promiseReadfile(){
+function promiseReadfile(pathFile){
   return new Promise((resolve, reject) => {
-    fs.readFile('accesstoken.txt', 'utf8', async (err, data) => {
+    fs.readFile(pathFile, 'utf8', async (err, data) => {
       if(err) reject(err);
       data = JSON.parse(data);
       if(data['expires_time'] < Date.now()){
@@ -51,10 +57,23 @@ function getCategories(){
     }
     let response = await fetch('https://public.kiotapi.com/categories', {headers:headers});
     let dataJson = await response.json();
-    let content = 'categoryId,parentId,categoryName,retailerId,hasChild,modifiedDate,createdDate,children'
+
+    let content = 'categoryId,parentId,categoryName,retailerId,hasChild,modifiedDate,createdDate\n';
+
     dataJson['data'].forEach(item => {
-      console.log(item,'test git pull');
-    })
+      content += item.categoryId   ? item.categoryId   + ',' : ',';
+      content += item.parentId     ? item.parentId     + ',' : ',';
+      content += item.categoryName ? item.categoryName + ',' : ',';
+      content += item.retailerId   ? item.retailerId   + ',' : ',';
+      content += item.hasChild     ? item.hasChild     + ',' : ',';
+      content += item.modifiedDate ? item.modifiedDate + ',' : ',';
+      content += item.createdDate  ? item.createdDate  : '';
+      content += '\n';
+    });
+    
+    fs.writeFile(fileCategories, content, err => {
+      if(err) throw err;
+    });
   });
 }
 
